5806943cfd2c427f353ef6af1dc9600c7a2dd452
Sina Kamrava <sinakamrava3118@gmail.com>
2023-10-14 08:26:25 +0330

    init the project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import QRCodeScanner from "./components/QRCodeScanner.vue";
+export default {
+  name: "app",
+  components: {
+    qrcodescanner: QRCodeScanner,
+  },
+};
+</script>
+
+<template>
+<main class="bg-slate-800 flex h-screen w-screen justify-center items-center">
+
+  <qrcodescanner />
+</main>
+</template>
---
+++ src/components/QRCodeScanner.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-1/2 h-1/2 rounded-lg overflow-hidden">
+    <qrcode-stream></qrcode-stream>
+  </div>
+</template>
+
+<script>
+import { QrcodeStream } from "vue-qrcode-reader";
+export default {
+  components: {
+    QrcodeStream,
+  },
+};
+</script>
